bb4efa55eeea4b1021f30afc2942e61632bbde40
Ashraf Soliman <ashraf.8466@gmail.com>
2022-11-15 14:32:19 +0200

    Stack Data Structure

--- stackDS/main.cpp
+++ stackDS/main.cpp
@@ -6,9 +6,78 @@
 //
 
 #include <iostream>
+using namespace std;
+
+const int MAX_SIZE = 100;
+
+template <class t>
+class Stack {
+    int top;
+    t items[MAX_SIZE];
+    
+public:
+    Stack(): top (-1) {}
+    
+    void push(int Element){
+        if (top>=MAX_SIZE - 1){
+            cout << "Stack full on push";
+        }
+        else {
+            top += 1;
+            items[top] = Element;
+        }
+    }
+    
+    bool isEmpty(){
+        return top < 0;
+    }
+    
+    void pop(){
+        if (isEmpty()){
+            cout << "Stack empty on pop";
+        }
+        else
+            top--;
+    }
+    
+    void pop(int& Element){
+        if (isEmpty()){
+            cout << "Stack empty on pop";
+        }
+        else {
+            Element = items[top];
+            top--;
+        }
+    }
+    
+    void removeElements (){
+        top = -1;
+    }
+    
+    void print (){
+        if (!isEmpty()) {
+            for (size_t i = 0; i <= top; i++){
+                cout << items[i] << "\t";
+            }
+            cout << endl;
+        } else {
+            cout << "Stack is empty!" << endl;
+        }
+    }
+};
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    Stack<int> stack;
+    stack.push(54);
+    stack.push(23);
+    stack.push(34);
+    stack.pop();
+    stack.push(12);
+    stack.print();
+    stack.removeElements();
+    stack.print();
+    
     return 0;
 }
