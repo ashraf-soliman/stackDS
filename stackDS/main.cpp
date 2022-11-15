2b9cdb2e12474093de39004eebc92c68cfb41e18
Ashraf Soliman <ashraf.8466@gmail.com>
2022-11-15 14:17:01 +0200

    Initial Commit

---
+++ stackDS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  stackDS
+//
+//  Created by ashraf on 15/11/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
